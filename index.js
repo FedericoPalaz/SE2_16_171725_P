9dc682010641ff5e85cd176f6794c81876b4778f
fruttasecca <jacopo.gobbi@studenti.unitn.it>
2016-11-26 14:49:39 +0100

    setting heroku
    setting heroku

--- index.js
+++ index.js
@@ -3,14 +3,27 @@
 //dependencies
 
 var express = require('express');//express library
+var pg = require('pg');//postgre database
 var path = require('path');//for pathing stuff
 var util = require('util');//to inspect stuff
 var url = require('url');//parse url
 var bodyParser = require('body-parser');//to parse stuff
 
-
-//creating instance
 var app = express();
+
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
+
 
 //setting port
 app.set('port', (process.env.PORT || 1337));
